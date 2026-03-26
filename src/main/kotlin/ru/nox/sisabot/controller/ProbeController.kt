c5890bf22aae10c58163142ee48dc9778e46a337
k.shchegolev <k.shchegolev@bft.ru>
2026-03-26 19:34:50 +0300

    add probe

---
+++ src/main/kotlin/ru/nox/sisabot/controller/ProbeController.kt
@@ -0,0 +1,20 @@
+import jakarta.annotation.PostConstruct
+import org.springframework.web.bind.annotation.*
+import org.telegram.telegrambots.meta.api.objects.Update
+import ru.nox.sisabot.controller.BotController
+import java.time.LocalDateTime
+
+@RestController
+class ProbeController() {
+
+    @PostConstruct
+    fun Initialization() {
+        println("Application started!")
+    }
+
+    // Добавим GET для проверки, что контроллер вообще работает
+    @PostMapping("/ping")
+    fun test(): String {
+        return "Webhook endpoint is alive! Use POST for Telegram updates."
+    }
+}
